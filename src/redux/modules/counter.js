fa2c5d4f34045a11eb6c8f0706a1e8550f9f8e22
Johan Gustafsson <johan.gustafsson@solidio.se>
2016-03-31 10:46:18 +0200

    First commit. Changed some configurations

--- src/redux/modules/counter.js
+++ src/redux/modules/counter.js
@@ -2,7 +2,7 @@
 // ------------------------------------
 // Constants
 // ------------------------------------
-export const COUNTER_INCREMENT = 'COUNTER_INCREMENT'
+export const COUNTER_INCREMENT = 'COUNTER_INCREMENT';
 
 // ------------------------------------
 // Actions
@@ -16,7 +16,7 @@ export function increment (value: number = 1): Action {
   return {
     type: COUNTER_INCREMENT,
     payload: value
-  }
+  };
 }
 
 // This is a thunk, meaning it is a function that immediately
@@ -29,31 +29,31 @@ export const doubleAsync = (): Function => {
   return (dispatch: Function, getState: Function): Promise => {
     return new Promise((resolve: Function): void => {
       setTimeout(() => {
-        dispatch(increment(getState().counter))
-        resolve()
-      }, 200)
-    })
-  }
-}
+        dispatch(increment(getState().counter));
+        resolve();
+      }, 200);
+    });
+  };
+};
 
 export const actions = {
   increment,
   doubleAsync
-}
+};
 
 // ------------------------------------
 // Action Handlers
 // ------------------------------------
 const ACTION_HANDLERS = {
   [COUNTER_INCREMENT]: (state: number, action: {payload: number}): number => state + action.payload
-}
+};
 
 // ------------------------------------
 // Reducer
 // ------------------------------------
-const initialState = 0
+const initialState = 0;
 export default function counterReducer (state: number = initialState, action: Action): number {
-  const handler = ACTION_HANDLERS[action.type]
+  const handler = ACTION_HANDLERS[action.type];
 
-  return handler ? handler(state, action) : state
+  return handler ? handler(state, action) : state;
 }
