fdec1147e6cff693a536f048ef751260e68711d6
Johan Gustafsson <johan.gustafsson@solidio.se>
2016-04-03 17:45:06 +0200

    Added test files

---
+++ test/middleware/test.apiMiddleware.js
@@ -0,0 +1,5 @@
+import test from 'ava';
+import sinon from 'sinon';
+import * as mw from '../../src/redux/middleware/apiMiddleware';
+
+test.todo('Write tests for apiMiddleware');
---
+++ test/modules/statuses/test.statusProgress.js
@@ -0,0 +1,8 @@
+import test from 'ava';
+import sinon from 'sinon';
+import thunk from 'redux-thunk';
+import * as actions from '../../../src/redux/modules/statuses/statusProgress';
+import { status } from '../../../src/redux/modules/statuses/statusProgress';
+import * as statuses from '../../../src/redux/modules/statuses/statuses';
+
+test.todo('Write tests for statusProgress');
